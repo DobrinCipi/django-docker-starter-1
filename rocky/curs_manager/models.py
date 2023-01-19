a9a001195f818f841c0742e8b3b92810957f533f
Dobrin Ciprian <cipi_d08@yahoo.com>
2023-01-19 13:41:31 +0200

    Validare caractere speciale

--- rocky/curs_manager/models.py
+++ rocky/curs_manager/models.py
@@ -8,13 +8,23 @@ def validate_no_a(value):
     for i in string.ascii_lowercase:
         if i in value:
             raise ValidationError("Caracter nepermis")
+        
+def validate_no_sepecial_characters(value):
+    restrition_list = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
+    for a in restrition_list:
+        raise ValidationError("Nu sunt persmise caractere speciale")
+    
+def validate_no_num_in_name(value):
+    list_number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    for a in list_number:
+        raise ValidationError("Nu sunt permise cifre")
 
 # Create your models here.
 class Student(models.Model):
-    nume = models.TextField()
-    prenume = models.TextField()
+    nume = models.TextField(validators=[validate_no_num_in_name, validate_no_sepecial_characters,])
+    prenume = models.TextField(validators=[validate_no_num_in_name, validate_no_sepecial_characters,])
     an = models.IntegerField(default=1, db_index=True)
     telefon = models.TextField(null=True, blank=True)
     cnp = models.CharField(unique=True, null=True, blank=True, max_length=13,
-                                validators=[MinLengthValidator(13), validate_no_a])
+                                validators=[MinLengthValidator(13), validate_no_a, validate_no_sepecial_characters,])
             
