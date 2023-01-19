3f880c6c83456c88e5571f63c0390e755a36fd87
Dobrin Ciprian <cipi_d08@yahoo.com>
2023-01-19 13:27:12 +0200

    Validation no letters in cnp

--- rocky/curs_manager/models.py
+++ rocky/curs_manager/models.py
@@ -1,11 +1,13 @@
 from django.core.exceptions import ValidationError
 from django.core.validators import MinLengthValidator
 from django.db import models
+import string
 
 
 def validate_no_a(value):
-    if "a" in value:
-        raise ValidationError("Caracter nepermis")
+    for i in string.ascii_lowercase:
+        if i in value:
+            raise ValidationError("Caracter nepermis")
 
 # Create your models here.
 class Student(models.Model):
